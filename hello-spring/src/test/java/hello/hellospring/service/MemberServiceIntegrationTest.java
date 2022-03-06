cac59e92f175ec5ff09294ef2f8c46edcee82f47
coding_egg <yjh9360@naver.com>
2022-03-06 15:03:06 +0900

    스프링 입문: 스프링 JdbcTemplate

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -48,7 +48,5 @@ class MemberServiceIntegrationTest {
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-
-
     }
 }
